536199d60424fff80fb89a2604143cc98183dc0e
Saul Johnson <saul.a.johnson@gmail.com>
2024-05-15 22:46:28 +0300

    Add setup script

---
+++ setup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Install required software.
+apt-get update
+apt-get install -y openssh-server python3 python3-pip dos2unix
+
+# Copy project to container.
+cd ../
+cp -r ./limbosh /etc/limbosh
+cd ./limbosh
+
+# Fix line endings in entrypoint files, then source files.
+dos2unix /etc/limbosh/limbosh*
+find /etc/limbosh -type f -name '*.py' -print0 | xargs -0 dos2unix --
+
+# Set appropriate permissions, create symlinks, register shell.
+chmod +x /etc/limbosh/limbosh-docker
+ln -s /etc/limbosh/limbosh-docker /usr/bin/limbosh
+chmod +x /usr/bin/limbosh
+echo '/usr/bin/limbosh' >> /etc/shells
+
+# Set up Python dependencies.
+cd /etc/limbosh
+pip install --upgrade pip
+pip install -r requirements.txt
+
+# Add admin:admin honeypot user with limbosh as shell.
+useradd -rm -d /home/admin -s /usr/bin/limbosh -g root -G sudo -u 1000 admin && echo 'admin:admin' | chpasswd
